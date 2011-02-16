5db10b07ea818817c055512addac264f76c7c79b
Aditya Yadav <aditya@netroy.in>
2011-02-16 11:29:41 +0530

    added socket.io to server.js

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 var http = require('http');
-http.createServer(function (req, res) {
+var io = require('socket.io');
+
+var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\nApp (netroy) is running..');
-}).listen(8586);
+});
+server.listen(8586);
+
+var socket = io.listen(server);
+socket.on('connection', function(client){
+  client.broadcast({announcement: client.sessionId + ' connected'});
+
+  client.on('message', function(message){
+    var msg = { message: [client.sessionId, message] };
+    client.broadcast(msg);
+  });
+
+  client.on('disconnect', function(){
+    client.broadcast({ announcement: client.sessionId + ' disconnected' })
+  });
+});
