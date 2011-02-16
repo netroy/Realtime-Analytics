c31951acfa8128cae6eb4d5536d9ef483e7ddd44
Aditya Yadav <aditya@netroy.in>
2011-02-16 17:40:48 +0530

    added geoip support

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
+var list = document.getElementById("container").getElementsByTagName("ul")[0];
+function addToList(hit){
+  console.log(hit);
+}
+
 var socket = new io.Socket(null, {port: location.port, rememberTransport: false});
-socket.connect();
-socket.on("connect",function(){console.log("connected");})
-socket.on('message', function(obj){
-  console.log(obj);
+socket.on('message', function(messages){
+  if('backlog' in messages){
+    for(var msg in messages.backlog) addToList(messages.backlog[msg]);
+  }else{
+    addToList(messages);
+  }
 });
+socket.connect();
 
--- server.js
+++ server.js
@@ -2,34 +2,49 @@ var http = require('http');
 var io = require('socket.io');
 var fs = require('fs');
 var url = require('url');
+var geoip = require('geoip');
 
 var mimeMap = {
   "html":"text/html",
   "css":"text/css",
   "js":"application/javascript"
 };
-
 var hitMap = []
+
+// Load Geodata
+var geoData = geoip.open(__dirname + '/data/GeoLiteCity.dat');
 
 var server = http.createServer(function (req, res) {
   var path = url.parse(req.url).pathname;
   if(path === '/') path='/index.html';
+
+  // Handle the beacon
   if(path === '/beacon'){
+    var ip = req.connection.remoteAddress;
+    var city = geoip.City.record_by_addr(geoData,ip);
+
     var hitObj = {
-      "ip":req.connection.remoteAddress,
-      "referer":req.headers.referer
+      "ip":ip,
+      "referer":req.headers.referer,
+      "city":city
     };
     hitMap.push(hitObj);
 
+    // return blank response for beacon
     if(hitMap.length > 10) hitMap.shift();
     res.writeHead(200);
     res.end();
+
+    // broadcast to all connected folks about the new beacon
     socket.broadcast(hitObj);
+    console.log(hitObj);
     return;
   }
 
+  // looks like someone requested a file .. server it if it exists
   fs.readFile(__dirname + path, 'utf8', function(err, data){
     if(err){
+      // looks like file doesn't exist .. 404
       res.writeHead(404);
       res.write('404 - "'+path+'" Not Found');
     }else{
@@ -39,12 +54,14 @@ var server = http.createServer(function (req, res) {
     }
     res.end();
   });
-
 });
 server.listen(8586);
 
+// Time to create a websocket listener for the server
 var socket = io.listen(server);
 socket.on('connection', function(client){
+  client.send({ "backlog": hitMap });
+/*
   //client.broadcast({announcement: client.sessionId + ' connected'});
 
   client.on('message', function(message){
@@ -55,5 +72,6 @@ socket.on('connection', function(client){
   client.on('disconnect', function(){
     //client.broadcast({ announcement: client.sessionId + ' disconnected' })
   });
+*/
 });
 
